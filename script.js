30c9ff8ad8d3e76df37258de7894fa2f2db2d49d
hannahliu123 <hannah.liu2010@gmail.com>
2025-05-25 17:14:53 -0400

    add/remove text feature

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.getElementById('text-input');
+const addButton = document.getElementById('add-button');
+const textContainer = document.getElementById('text-container');
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const addText = document.createElement('p');
+        addText.textContent = input.value;
+        textContainer.append(addText);
+    }
+});
+
+addButton.addEventListener('click', () => {
+    const addText = document.createElement('p');
+    addText.textContent = input.value;
+    textContainer.append(addText);
+});
+
+textContainer.addEventListener('click', (event) => {
+    if (event.target.tagName === 'P') { // so you don't delete the div (container)
+        event.target.remove();
+    }
+});
