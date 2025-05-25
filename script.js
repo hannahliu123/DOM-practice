1f64db164130153a8a21ab2cbabe891b9244683f
hannahliu123 <hannah.liu2010@gmail.com>
2025-05-25 17:39:15 -0400

    start color change

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const input = document.getElementById('text-input');
 const addButton = document.getElementById('add-button');
 const textContainer = document.getElementById('text-container');
+const colorContainer = document.getElementById('change-color-container');
 
-document.addEventListener('keydown', (event) => {
+input.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         const addText = document.createElement('p');
         addText.textContent = input.value;
@@ -19,5 +20,11 @@ addButton.addEventListener('click', () => {
 textContainer.addEventListener('click', (event) => {
     if (event.target.tagName === 'P') { // so you don't delete the div (container)
         event.target.remove();
+    }
+});
+
+colorContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('shape')) {
+        alert(event.target);    // UNFINISHED
     }
 });
