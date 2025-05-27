6e57e91b6b3c5bfb770a653a3705cc982a2300f8
hannahliu123 <hannah.liu2010@gmail.com>
2025-05-27 17:53:41 -0400

    sleep & organize

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
+// ========== Add Text Section ==========
 const addInput = document.getElementById('text-input');
 const addButton = document.getElementById('add-button');
 const textContainer = document.getElementById('text-container');
+
+// ========== Shapes Color Section ==========
 const colorContainer = document.getElementById('change-color-container');
+
+// ========== Character Counter ==========
 const charCountInput = document.getElementById('char-count-input');
 const charDisplay = document.getElementById('char-display');
+
+// ========== Habit Checkboxes ==========
 const checkboxContainer = document.getElementById('checkboxes');
 const pointsDisplay = document.getElementById('habits-points');
 const pointsMessage = document.getElementById('points-message');
+const sleepHoursContainer = document.getElementById('sleep-hours-container');
 
-addInput.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        const addText = document.createElement('p');
-        addText.textContent = addInput.value;
-        textContainer.append(addText);
-        addInput.value = '';
-    }
-});
-
-addButton.addEventListener('click', () => {
+function addTextToContainer() {
     const addText = document.createElement('p');
     addText.textContent = addInput.value;
     textContainer.append(addText);
     addInput.value = '';
+}
+
+addInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') addTextToContainer();
 });
+
+addButton.addEventListener('click', addTextToContainer());
 
 textContainer.addEventListener('click', (event) => {
     if (event.target.tagName === 'P') { // so you don't delete the div (container)
@@ -45,12 +50,22 @@ checkboxContainer.addEventListener('change', (event) => {
     const checkbox = event.target;
     if (checkbox.checked) {
         totalPoints += parseInt(checkbox.dataset.points); // Convert string into integer
+        // show hours of sleep input
+        if (checkbox.id === 'get-sleep') {
+            sleepHoursContainer.style.display = 'block';
+        }
     } else {
-        totalPoints -= parseInt(checkbox.dataset.points); // Convert string into integer
-    } 
+        if (checkbox.type === 'checkbox') {
+            totalPoints -= parseInt(checkbox.dataset.points); // Convert string into integer
+            // hide hours of sleep input
+            if (checkbox.id === 'get-sleep') {
+                sleepHoursContainer.style.display = 'none';
+            }
+        }
+    }
     
     pointsDisplay.textContent = totalPoints;
-    if (totalPoints >= 70) {
+    if (totalPoints > 70) {
         pointsMessage.textContent = 'Amazing Job! Keep it up!';
         pointsMessage.classList.add('three');
     } else if (totalPoints > 40) {
